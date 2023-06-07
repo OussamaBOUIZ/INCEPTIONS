44a1fe6f2972333f50b4ef157cc9a099792b5681
root <root@debianHostname.debiandomainname>
2023-06-07 13:39:38 -0400

    last version

--- rendu/srcs/requirements/mariadb/tools/script.sh
+++ rendu/srcs/requirements/mariadb/tools/script.sh
@@ -16,5 +16,5 @@ echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db.sql
 echo "FLUSH PRIVILEGES;" >> db.sql
 
 mysql < db.sql
-# service mysql stop
+service mysql stop
 mysqld
--- rendu/srcs/requirements/wordpress/script.sh
+++ rendu/srcs/requirements/wordpress/script.sh
@@ -5,7 +5,6 @@ mkdir -p /var/www/html
 
 cd /var/www/html
 
-# rm -rf *
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 
